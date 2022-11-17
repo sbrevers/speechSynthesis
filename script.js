bc05ee45da5dfd19b2c9460e8cb8c84863a0d361
sbrevers <bsp001@hotmail.com>
2022-11-17 15:05:41 +0100

    Option

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
-
-msg.text = document.querySelector("[name='text']").value;
+console.log(msg);
 // 5 addEvenlistener + 5 fonctions
 
-function speaker() {
-  speechSynthesis.speak(msg);
+function speaker(start = true) {
+  msg.text = document.querySelector("[name='text']").value;
+  speechSynthesis.cancel();
+  if (start) {
+    speechSynthesis.speak(msg);
+    console.log(msg);
+  }
+}
+
+function changeVoice() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name}(${voice.lang})</option>`
+    )
+    .join("");
 }
 
 speakButton.addEventListener("click", speaker);
 
 stopButton.addEventListener("click", () => {
-  speechSynthesis.cancel();
+  speaker(false);
 });
+
+speechSynthesis.addEventListener("voiceschanged", changeVoice);
